7973a474e19287bf8983b3ac0dce0230563ea96e
jlee9503 <jungsu9503@gmail.com>
2021-01-16 23:30:18 -0500

    first commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// Selectors for all buttons
+const numberBtns = document.querySelectorAll("num");
+const operatorBtns = document.querySelectorAll("operator");
+const numberBtns = document.querySelectorAll("tool");
+
+// Calculator Class
+
+
+// Event Listeners
+numberBtns.forEach((btn) => {
+  btn.addEventListener
+})
