39c34c04caad4896a41f0d07d13a03c15f1084f1
jlee9503 <jungsu9503@gmail.com>
2021-01-19 23:17:40 -0500

    Add JavaScript features (display numbers & all clear button)

--- calculator.js
+++ calculator.js
@@ -1,12 +1,56 @@
 // Selectors for all buttons
-const numberBtns = document.querySelectorAll("num");
-const operatorBtns = document.querySelectorAll("operator");
-const numberBtns = document.querySelectorAll("tool");
+const numberBtns = document.querySelectorAll(".num");
+const operatorBtns = document.querySelectorAll("#num-operator");
+const allClearBtn = document.querySelector("#all-clear");
+const deleteBtn = document.querySelector("#delete");
+const equalBtn = document.querySelector("#equal-btn");
+const outputText = document.querySelector(".answer");
 
-// Calculator Class
+//Calculator Class
+class Calculator {
+  constructor(outputText) {
+    this.outputText = outputText;
+    this.clear();
+  }
 
+  clear() {
+    this.output = '';
+    this.operator = undefined;
+  }
+
+  delete() {
+
+  }
+
+  appendNum(number) {
+    this.output = this.output + number;
+  }
+
+  selectOperator(operator) {
+
+  }
+
+  operation() {
+
+  }
+
+  updateDisplay() {
+    this.outputText.innerText = this.output;
+  }
+}
+
+// Create a new calculator class
+const calc = new Calculator(outputText);
 
 // Event Listeners
 numberBtns.forEach((btn) => {
-  btn.addEventListener
-})
+	btn.addEventListener("click", () => {
+    calc.appendNum(btn.innerText);
+    calc.updateDisplay();
+	});
+});
+
+allClearBtn.addEventListener('click', (btn) => {
+  calc.clear();
+  calc.updateDisplay();
+});
